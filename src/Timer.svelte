2042ee79f1163005aad3bfecb36763dac0f3aee8
David Garcia Perez <davidgape89@gmail.com>
2021-01-17 18:25:50 +0100

    Initial commit

---
+++ src/Timer.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    export let isRunning = false;
+    let time = 50;
+
+    function reset() {
+        time = 0;
+    }
+
+    function formatTime(t: number): string {
+        const timeString = t.toString();
+        return timeString.length === 1 ? `0${timeString}` : timeString;
+    }
+
+    onMount(() => {
+        const interval = setInterval(() => {
+            if(isRunning) time += 1
+        }, 1000);
+        return () => clearInterval(interval);
+    });
+</script>
+
+<div>{formatTime(Math.trunc(time / 60))}:{formatTime(time % 60)}</div>
